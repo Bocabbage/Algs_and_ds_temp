d211f75c401ba3b8fb60242927dda7d2d15d6eb5
Bocabbage <378066505@qq.com>
2019-09-21 10:43:45 +0800

    Debug IFFT. Finish recursive-version of FFT/IFFT.

--- py_version/Polynomial_Mult/FFT.py
+++ py_version/Polynomial_Mult/FFT.py
@@ -25,7 +25,7 @@ def Recursive_FFT(arr):
     y1 = Recursive_FFT(a_1)
 
     Omega = 1
-    Omega_n = exp(-2*pi*1j/Len)
+    Omega_n = exp(2*pi*1j/Len)
 
     res_y = [ 0 for x in range(Len) ]
 
@@ -36,7 +36,7 @@ def Recursive_FFT(arr):
 
     return res_y
 
-def Recursive_IFFT(y,cut=1e-6):
+def Recursive_IFFT(y,cut=1e-10):
     '''
         input   : [list] of fft results
         output  : [list] of coefficients
@@ -52,31 +52,33 @@ def Recursive_IFFT(y,cut=1e-6):
     y_0 = [ y[idx] for idx in range(Len) if idx % 2 ==0 ]
     y_1 = [ y[idx] for idx in range(Len) if idx % 2 ==1 ]
 
-    print("{}:y0:{}".format(Len,y_0))
-    print("{}:y1:{}".format(Len,y_1))
+    # print("{}:y0:{}".format(Len,y_0))
+    # print("{}:y1:{}".format(Len,y_1))
 
     a0 = Recursive_IFFT(y_0)
     a1 = Recursive_IFFT(y_1)
 
-    print("{}:a0:{}".format(Len,a0))
-    print("{}:a1:{}".format(Len,a1))
-
     I_Omega = 1
-    I_Omega_n = exp(2*pi*1j/Len)
+    I_Omega_n = exp(-2*pi*1j/Len)
 
     res_a = [ 0 for x in range(Len) ]
 
-    for k in range(Len//2):
-        res_a[k] = (1/Len) * (a0[k]+I_Omega*a1[k])
-        res_a[k+Len//2] = (1/Len) * (a0[k]-I_Omega*a1[k])
+    for j in range(Len//2):
+        # Attention: the recursive-function format:
+        # a[j] = (1/Len) *(A0[j]+I_Omega*A1[j]) 
+        # while Ax[j] = 0.5*Len*ax[j]
+        res_a[j] = 0.5 * (a0[j]+I_Omega*a1[j])
+        res_a[j+Len//2] = 0.5 * (a0[j]-I_Omega*a1[j])
         I_Omega *= I_Omega_n
 
-    CUT= lambda x: x.real if x.imag < cut else x
+    CUT= lambda x: x.real if abs(x.imag) < cut else x
+    # print("a_{}:{}".format(Len,res_a))
+    # print("a_{}_cut:{}".format(Len,[CUT(x) for x in res_a]))
     return [CUT(x) for x in res_a]
 
 
 if __name__ == '__main__':
-    x = [x for x in range(4)]
+    x = [x for x in range(8)]
     y = Recursive_FFT(x)
     rx = Recursive_IFFT(y)
     print("x:{}".format(x))
